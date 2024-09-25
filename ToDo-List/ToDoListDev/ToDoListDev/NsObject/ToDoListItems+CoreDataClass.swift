cc8a5232bdbb1c278a5f42149ce79f9fd9670875
Tapan Sethi <contact.tapansethi@gmail.com>
2024-09-25 10:08:04 +0530

    Add files via upload
    This is the first commit

---
+++ ToDo-List/ToDoListDev/ToDoListDev/NsObject/ToDoListItems+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItems+CoreDataClass.swift
+//  ToDoListDev
+//
+//  Created by iOS Developer on 04/12/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItems)
+public class ToDoListItems: NSManagedObject {
+
+}
